7f6d87c2ae1dc3d87d36aab47e658e9756210f4c
LIUWENCAN <pheye@qq.com>
2019-11-17 17:06:06 +0800

    Updates

--- demo2/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "normalize.css/normalize.css";
-import "font-awesome/css/font-awesome.min.css";
-import "./index.css";
-
-ReactDOM.render(<App />, document.getElementById("app"));
--- readme.md
+++
@@ -1,4 +0,0 @@
-# React 练习
-- demo01 demo01为《React基础》的源码
-- demo2 demo2为《前端工程化》的源码
-
