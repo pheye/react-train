7bb6895abf99d8c308260605385074b9f4be5ac5
LIUWENCAN <pheye@qq.com>
2019-11-26 10:41:56 +0800

    调整代码结构，以便支持多应用

--- demo4/src/index.js
+++ demo4/src/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import reducers from './reducers'
 
-const store = createStore(reducers,
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import Todos from './todos/index';
 
-ReactDOM.render(<Provider store={store}>
-    <App />
-    </Provider>, 
+ReactDOM.render(<Todos />, 
     document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
