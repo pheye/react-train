5f5e086627d5bb6ded3bdb606a8d4ea828ef9e6f
LIUWENCAN <pheye@qq.com>
2019-12-03 12:22:41 +0800

    使用dva-loading解决异步请求的正在加载的问题

--- dva-shopping-cart/src/components/Cart.js
+++ dva-shopping-cart/src/components/Cart.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'dva';
 
 const Cart = props => {
-  const { products, subtotal, onCheckout, loading } = props;
+  const { products, subtotal, onCheckout, loading, checking } = props;
   const nodes = products.map((item, key) => (
     <li key={key}>
       {item.title} {item.price} x {item.quantity}
@@ -14,15 +14,18 @@ const Cart = props => {
       <ul>{nodes}</ul>
       <div>Total: {subtotal}</div>
       <div>
+        {checking && <div style={{color: 'red'}}>Checking ...</div>}
         <button onClick={onCheckout} disabled={subtotal <= 0.00 || loading}>Checkout</button>
       </div>
     </div>
   );
 };
 
-const mapStateToProps = ({cart, products}) => ({
+const mapStateToProps = ({cart, products, loading}) => ({
   products: cart.added.map(id => ({...products.byId[id], quantity: cart.quantities[id]})),
-  subtotal: cart.added.reduce((amount, id) => products.byId[id].price * cart.quantities[id] + amount, 0).toFixed(2)
+  subtotal: cart.added.reduce((amount, id) => products.byId[id].price * cart.quantities[id] + amount, 0).toFixed(2),
+  loading: loading.models['cart'],
+  checking: loading.effects['cart/checkout']
 })
 
 const mapDispatchToProps = (dispatch) => ({
--- dva-shopping-cart/src/index.js
+++ dva-shopping-cart/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import dva from 'dva';
+import createLoading from 'dva-loading';
 import App from './components/App';
 import products from './models/products';
 import cart from './models/cart';
@@ -7,5 +8,6 @@ import cart from './models/cart';
 const app = dva();
 app.model(products);
 app.model(cart);
+app.use(createLoading());
 app.router(() => <App />);
 app.start('#root');
